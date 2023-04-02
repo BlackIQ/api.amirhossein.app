0b99d86f50d1ad5bfb8c725254043b823ff6b7de
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-02 03:35:06 +0330

    Installed axios and add get additionals

--- src/controllers/services/services.controller.js
+++ src/controllers/services/services.controller.js
@@ -1,18 +1,41 @@
 import pm2 from "pm2";
+import axios from "axios";
+
+const additionals = [
+  {
+    name: "Shahab Khodro",
+    baseUrl: "http://shahab.narbon.ir:15160/v1/srvices",
+  },
+];
 
 export const ALL = async (req, res) => {
+  const data = {};
+
+  await Promise.all(
+    additionals.map(async (additional) => {
+      try {
+        const response = await axios.get(additional.baseUrl);
+
+        data[additional.name] = response.data;
+      } catch (errpr) {
+        data[additional.name] = false;
+      }
+    })
+  );
+
   pm2.list((error, list) => {
     if (error) {
       res.status(200).send({ message: error.message });
     }
 
-    res.status(200).send(
-      list.sort((a, b) => {
+    res.status(200).send({
+      in: list.sort((a, b) => {
         if (a.name < b.name) return -1;
         if (a.name > b.name) return 1;
         return 0;
-      })
-    );
+      }),
+      out: data,
+    });
   });
 };
 
